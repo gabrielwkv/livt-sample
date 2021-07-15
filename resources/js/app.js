0e90010333ff10d4898dc4e8a5224c9d9841a1d5
Gabriel M. dos Santos <gabrielwkv@MacBook-Pro-de-Gabriel.local>
2021-07-15 09:08:30 -0500

    inertia client-side configuration

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue'
+
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
+
+createInertiaApp({
+    resolve: name => import(`./pages/${name}`),
+    setup({ el, app, props }) {
+        new Vue({
+            render: h => h(app, props),
+        }).$mount(el)
+    },
+})
